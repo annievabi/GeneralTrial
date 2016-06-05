2e5c01d9e5405b3498a8a46af3fab47340598ca4
hasan <hasan@yahoo.com>
2016-06-05 17:07:53 -0400

    this is the second changes

--- GitsTrail/src/com/gitpackage/GitsTrailClass.java
+++ GitsTrail/src/com/gitpackage/GitsTrailClass.java
@@ -5,6 +5,7 @@ public class GitsTrailClass {
 	public static void main(String[] args) {
 		
 		System.out.println("this is adding two numbers");
+		System.out.println("this is the second changes");
 	}
 
 }
